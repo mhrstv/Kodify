71816405e7ab498df521cf7294235dd0aad10d90
Martin Hristov <entrydev.gm@gmail.com>
2025-01-23 00:15:49 +0200

    fix: remove unused vars

--- Kodify/AutoDoc/Models/DocumentationModel.cs
+++ Kodify/AutoDoc/Models/DocumentationModel.cs
@@ -6,6 +6,5 @@ namespace Kodify.AutoDoc.Models
         public string Summary { get; set; }
         public string Parameters { get; set; }
         public string ReturnType { get; set; }
-        public string Example { get; set; }
     }
 }
